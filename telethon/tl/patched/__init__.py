a964da66938e6ae021d1a70e234c03868c6a19ec
Devesh Pal <newdev0@outlook.com>
2023-02-20 00:20:36 +0530

    Patch User object

--- telethon/tl/patched/__init__.py
+++ telethon/tl/patched/__init__.py
@@ -1,20 +1,31 @@
-from .. import types, alltlobjects
+from .. import types, alltlobjects, functions
 from ..custom.message import Message as _Message
+from ..custom.user import User as USER
 
 class MessageEmpty(_Message, types.MessageEmpty):
     pass
 
+
 types.MessageEmpty = MessageEmpty
 alltlobjects.tlobjects[MessageEmpty.CONSTRUCTOR_ID] = MessageEmpty
+
 
 class MessageService(_Message, types.MessageService):
     pass
 
+
 types.MessageService = MessageService
 alltlobjects.tlobjects[MessageService.CONSTRUCTOR_ID] = MessageService
+
 
 class Message(_Message, types.Message):
     pass
 
+class User(USER, types.User):
+    pass
+
 types.Message = Message
 alltlobjects.tlobjects[Message.CONSTRUCTOR_ID] = Message
+
+types.User = User
+alltlobjects.tlobjects[User.CONSTRUCTOR_ID] = User
