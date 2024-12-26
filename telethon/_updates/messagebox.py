70f1415bf0a3326d068ce8e6ca24356b2204aebc
lanowde <117498666+lanowde@users.noreply.github.com>
2024-12-26 14:15:36 +0530

    layer 195

--- telethon/_updates/messagebox.py
+++ telethon/_updates/messagebox.py
@@ -329,6 +329,8 @@ class MessageBox:
     #
     # It also updates the next deadline time to reflect the new closest deadline.
     def reset_deadlines(self, entries, deadline):
+        if not entries:
+            return
         for entry in entries:
             if entry not in self.map:
                 raise RuntimeError(
