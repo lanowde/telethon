f0e701b728e227dc566491ca7caae5f3b5add414
lanowde <117498666+lanowde@users.noreply.github.com>
2025-07-28 10:10:33 +0530

    another' one

--- telethon/tl/custom/message.py
+++ telethon/tl/custom/message.py
@@ -223,6 +223,7 @@ class Message(ChatGetter, SenderGetter, TLObject):
         reactions_are_possible: Optional[bool] = None,
         paid_suggested_post_stars: Optional[bool] = None,
         paid_suggested_post_ton: Optional[bool] = None,
+        suggested_post: Optional[bool] = None,
     ):
         # Copied from Message.__init__ body
         self.id = id
@@ -271,6 +272,7 @@ class Message(ChatGetter, SenderGetter, TLObject):
         self.reactions_are_possible = reactions_are_possible
         self.paid_suggested_post_stars = paid_suggested_post_stars
         self.paid_suggested_post_ton = paid_suggested_post_ton
+        self.suggested_post = suggested_post
 
         # Convenient storage for custom functions
         # TODO This is becoming a bit of bloat
