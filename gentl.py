db6343c43e02530cf1a6113240fd8fcf3a1c2a01
lanowde <117498666+lanowde@users.noreply.github.com>
2026-04-15 07:33:29 +0530

    fmt'd

--- gentl.py
+++ gentl.py
@@ -1,4 +1,5 @@
 from hatch_build import CustomBuildHook
+
 tl_builder = CustomBuildHook()
-tl_builder.root = tl_builder.directory = '.'
-tl_builder.initialize('v1', { 'force_include': {} })
+tl_builder.root = tl_builder.directory = "."
+tl_builder.initialize("v1", {"force_include": {}})
